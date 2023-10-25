04a4d1eab6f1bb90e7fc97877a8b5306eabe33ec
mrpersimmon <2218443280@qq.com>
2023-10-26 03:53:50 +0800

    feat: 解耦 ClassPathXmlApplicationContext
    定义 XmlBeanDefinitionReader

---
+++ src/com/minispring/beans/XmlBeanDefinitionReader.java
@@ -0,0 +1,24 @@
+package com.minispring.beans;
+
+import org.dom4j.Element;
+
+/**
+ * 将解析好的 XML 转换成所需要的 BeanDefinition
+ */
+public class XmlBeanDefinitionReader {
+    BeanFactory beanFactory;
+
+    public XmlBeanDefinitionReader(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public void loadBeanDefinitions(Resource resource) {
+        while (resource.hasNext()) {
+            Element element = (Element) resource.next();
+            String beanId = element.attributeValue("id");
+            String beanClassName = element.attributeValue("class");
+            BeanDefinition beanDefinition = new BeanDefinition(beanId, beanClassName);
+            this.beanFactory.registerBeanDefinition(beanDefinition);
+        }
+    }
+}
