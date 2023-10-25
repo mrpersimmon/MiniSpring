aff2fe9f8140045a95f27e6487a5911407a684e8
mrpersimmon <2218443280@qq.com>
2023-10-26 04:27:27 +0800

    feat: 解耦 ClassPathXmlApplicationContext
    改造 ClassPathXmlApplicationContext，并验证功能

--- src/com/minispring/test/service/Test1.java
+++ src/com/minispring/test/service/Test1.java
@@ -1,9 +1,10 @@
 package com.minispring.test.service;
 
+import com.minispring.beans.BeansException;
 import com.minispring.beans.ClassPathXmlApplicationContext;
 
 public class Test1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         AService aservice = (AService) ctx.getBean("aservice");
         aservice.sayHello();
